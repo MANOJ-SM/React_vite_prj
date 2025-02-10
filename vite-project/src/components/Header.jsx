020f9cfe962a516d7acd0ac80c949e9d2647d068
MANOJ_SM <manumedleri@gmail.com>
2025-02-10 23:54:03 +0530

    `Added named export and default export in Header component`

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -8,3 +8,25 @@ const Header = () =>{
 
 
   export default  Header;
+
+//   export default  Header; - default export 
+
+// in one file u can xport only oone comp
+
+// while in named export u can export multiple comp from one file
+
+
+/*
+
+named export 
+
+export const Header = () =>{
+    return(
+      <>
+      <h1>hey i did it </h1>
+      </>
+    )
+  }
+
+  */
+
