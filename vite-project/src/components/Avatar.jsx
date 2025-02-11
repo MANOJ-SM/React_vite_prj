c2b43f24eec8eea3450ed8fa8b303953f2330ef3
MANOJ_SM <manumedleri@gmail.com>
2025-02-11 22:10:01 +0530

    aded to unserstand data transfer

---
+++ vite-project/src/components/Avatar.jsx
@@ -0,0 +1,9 @@
+function Avatar(props){
+    return (
+        <>
+        <img src={props.image} alt=" " height={props.size} width={props.size}/>  
+        </>
+    )
+}
+
+export default Avatar;
---
+++ vite-project/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import Avatar from "./Avatar";
+
+function Profile(){
+    return (
+        <>
+        <h1>profiles</h1>
+        <Avatar image="public/bill_gates.png" size="200"/>
+        <Avatar image="public/steve_job.png" size="200px" />
+        </>
+    )
+};
+
+
+export default Profile;
