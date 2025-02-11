6ae2cc8aeeed6ebd10dc9fab5c2c229ecbc91156
MANOJ_SM <manumedleri@gmail.com>
2025-02-11 07:55:15 +0530

    created new two components

---
+++ vite-project/src/components/ChildComponent.jsx
@@ -0,0 +1,11 @@
+function ChildComponent(){
+    return (
+        <>
+        <h1>ChildComponent</h1>
+        </>
+    )
+};
+
+
+export default ChildComponent;
+
---
+++ vite-project/src/components/ParentComponent.jsx
@@ -0,0 +1,9 @@
+function ParentComponent(){
+    retrun (
+        <>
+        <h1>ParentComponent</h1>
+        </>
+    )
+};
+
+export default ParentComponent;
